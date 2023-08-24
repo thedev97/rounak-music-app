1ecd7d76feca175b9bcfaff529b83538e538cd82
Soumya <soumyamishra637@gmail.com>
2023-08-25 00:37:27 +0530

    New implementation starts

--- lib/core/utils/neuCommonClass.dart
+++
@@ -1,4 +0,0 @@
-
-class CommonClass{
-
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'presentation/screens/splashScreen/splashScreen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
- // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown
-    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "",
-      home: SplashScreen(),
     );
   }
 }
